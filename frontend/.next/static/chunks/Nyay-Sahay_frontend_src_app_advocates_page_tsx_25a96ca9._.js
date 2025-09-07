840a9782e1d35c406e75c9920c171092bf1fe932
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-09-07 23:47:37 +0530

    edited

---
+++ frontend/.next/static/chunks/Nyay-Sahay_frontend_src_app_advocates_page_tsx_25a96ca9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/Nyay-Sahay_frontend_src_c1daf127._.js",
+  "static/chunks/044dc_e3a102ec._.js"
+],
+    source: "dynamic"
+});
---
+++ frontend/.next/static/chunks/Nyay-Sahay_frontend_src_app_features_page_tsx_25a96ca9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/Nyay-Sahay_frontend_src_71d36179._.js",
+  "static/chunks/044dc_920175bc._.js"
+],
+    source: "dynamic"
+});
